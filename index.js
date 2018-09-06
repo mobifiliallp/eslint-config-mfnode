a93dbda4a031c4a2cdecd9b34eb58e7a4fa75043
Gopal Raichur <gopal.raichur@mobifilia.com>
2018-09-06 17:04:59 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "extends": [
+    "airbnb-base"
+  ],
+  "rules": {
+    "no-underscore-dangle": "off",
+    "max-len": "off",
+    "object-shorthand": "off",
+    "prefer-destructuring": "off",
+    "no-param-reassign": "off",
+    "no-else-return": "off",
+    "no-prototype-builtins": "off",
+    "no-mixed-operators": [
+      "error",
+      {
+        "allowSamePrecedence": true
+      }
+    ]
+  }
+}
